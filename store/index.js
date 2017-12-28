53a0d435a7df9511c50cad49570ad25eb6457dde
Robert Gill <envman@gmail.com>
2017-12-28 11:01:01 +0000

    add mongo

--- store/index.js
+++ store/index.js
@@ -1,16 +1,30 @@
 const path = require('path')
 const fs = require('fs')
+const mongo = require('mongodb').MongoClient
 
 const express = require('express')
 
 const videoPath = path.join('/usr/src', 'videos')
+const mongoUrl = 'mongodb://video_mongo:27017/videos'
 
-const app = express()
-
-app.use('/static', express.static(videoPath))
-
-app.post('/upload', (req, res) => {
+mongo.connect(mongoUrl, (err, db) => {
+  if (err) {
+    return console.log(`Error Connecting to ${mongoUrl}`)
+  }
   
+  const app = express()
+  
+  app.use('/static', express.static(videoPath))
+
+  let videos = db.collection('videos')
+
+  app.post('/upload', (req, res) => {
+    // videos.insert()
+  })
+
+  app.listen(80)
 })
 
-app.listen(80)
+process.on('SIGTERM', function() {
+  app.stop()
+})
