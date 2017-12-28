f2cb32594de54143308a8aee55526db27330ee07
Steven Tsang <stevetsang@etech.net>
2017-12-28 12:10:25 +0000

    add database storage to stats

--- stats/app.js
+++ stats/app.js
@@ -2,11 +2,24 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
+const mongo = require('mongodb').MongoClient
 
-app.use(cors())
-app.use(bodyParser.json())
+const mongoUrl = 'mongodb://stats_mongo:27017/stats'
 
-const port = process.env.PORT || 3000
-app.listen(port)
+mongo.connect(mongoUrl, (err, db) => {
+  if (err) {
+    return console.log(`Error Connecting to ${mongoUrl}`)
+  }
 
-console.log(`viddy RESTful stats server started on: ${port}`)
+  app.use(cors())
+  app.use(bodyParser.json())
+
+  let stats = db.collection('stats')
+
+  const port = process.env.PORT || 3000
+  app.listen(port)
+
+  console.log(`viddy RESTful stats server started on: ${port}`)
+})
+
+process.on('SIGTERM', () => app.stop())
