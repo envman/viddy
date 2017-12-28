da453aba35b584c48095ed0ba51493b20c34f7b4
Steven Tsang <stevetsang@etech.net>
2017-12-28 11:35:46 +0000

    add body-parser and cors to stats service

--- stats/app.js
+++ stats/app.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const app = express()
-const port = process.env.PORT || 3000
 
+app.use(cors())
+app.use(bodyParser.json())
+
+const port = process.env.PORT || 3000
 app.listen(port)
 
-console.log('viddy RESTful stats server started on: ' + port)
+console.log(`viddy RESTful stats server started on: ${port}`)
